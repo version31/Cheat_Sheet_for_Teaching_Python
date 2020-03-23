7b55226af72681e959361bc5c4551321eaa4624b
Ali Sharifineyestani <sh4rifi@gmail.com>
2020-03-23 15:32:24 +0430

    minor modify

--- courses/10_tuple.py
+++ courses/10_tuple.py
@@ -2,11 +2,11 @@
 #  ordered and unchangeable
 
 # Access Tuple Items
-thistuple = ("apple", "banana", "cherry")
-print(thistuple[1])
-print(thistuple[-1])
-print(thistuple[2:5])
-print(thistuple[-4:-1])
+thisTuple = ("apple", "banana", "cherry")
+print(thisTuple[1])
+print(thisTuple[-1])
+print(thisTuple[2:5])
+print(thisTuple[-4:-1])
 
 # Change Tuple Values
 #  Tuples are unchangeable, or immutable as it also is called.
@@ -19,38 +19,38 @@ x = tuple(y)
 print(x)
 
 # Loop Through a Tuple
-thistuple = ("apple", "banana", "cherry")
-for x in thistuple:
+thisTuple = ("apple", "banana", "cherry")
+for x in thisTuple:
     print(x)
 
 # Check if Item Exists
-thistuple = ("apple", "banana", "cherry")
-if "apple" in thistuple:
+thisTuple = ("apple", "banana", "cherry")
+if "apple" in thisTuple:
     print("Yes, 'apple' is in the fruits tuple")
 
     # Tuple Length
-    thistuple = ("apple", "banana", "cherry")
-    print(len(thistuple))
+    thisTuple = ("apple", "banana", "cherry")
+    print(len(thisTuple))
 
 # Add Items
 # Once a tuple is created, you cannot add items to it. Tuples are unchangeable.
-thistuple[3] = "orange"  # This will raise an error
+thisTuple[3] = "orange"  # This will raise an error
 
 # Create Tuple With One Item
 # To create a tuple with only one item, you have add a comma after the item, unless Python will not recognize the variable as a tuple.
 
-thistuple = ("apple",)
-print(type(thistuple))  # // <class 'tuple'>
+thisTuple = ("apple",)
+print(type(thisTuple))  # // <class 'tuple'>
 
 # NOT a tuple
-thistuple = ("apple")
-print(type(thistuple))  # // <class 'str'>
+thisTuple = ("apple")
+print(type(thisTuple))  # // <class 'str'>
 
 # Remove Items
 # Note: You cannot remove items in a tuple.
 
-del thistuple  # # The del keyword can delete the tuple completely:
-print(thistuple)  # this will raise an error because the tuple no longer exists
+del thisTuple  # # The del keyword can delete the tuple completely:
+print(thisTuple)  # this will raise an error because the tuple no longer exists
 
 # The del keyword can delete the tuple completely:
 tuple1 = ("a", "b", "c")
@@ -60,18 +60,18 @@ tuple3 = tuple1 + tuple2
 print(tuple3)  # // ('a', 'b', 'c', 1, 2, 3)
 
 # The tuple() Constructor
-thistuple = tuple(("apple", "banana", "cherry"))
-print(thistuple)
+thisTuple = tuple(("apple", "banana", "cherry"))
+print(thisTuple)
 
 # Tuple Methods
 # count()
 # Returns the number of times a specified value occurs in a tuple
-thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
-x = thistuple.count(5)
+thisTuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thisTuple.count(5)
 print(x)
 
 # index()
 # Searches the tuple for a specified value and returns the position of where it was found
-thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
-x = thistuple.index(8) # // Search for the first occurrence of the value 8, and return its position:
-print(x) # // 3
+thisTuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thisTuple.index(8)  # // Search for the first occurrence of the value 8, and return its position:
+print(x)  # // 3
